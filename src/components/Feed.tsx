41fa8761bc1df4c9e7b996f255ab38cbe3b5f4e4
Oskar Nyberg <me@oskarnyberg.com>
2019-09-03 13:29:05 +0200

    Made it possible to select and play videos

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,6 +1,9 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useMemo, useState, useCallback } from 'react';
 import { FetcherContext } from './Fetcher';
 import { VideoData } from '../helpers/fetchFeed';
+import {ConfigContext} from './App';
+import {Config, saveLast} from '../config';
+import {playVideos} from '../helpers/player';
 
 class FeedRow implements VideoData {
   public title: string;
@@ -17,6 +20,10 @@ class FeedRow implements VideoData {
     this.date = data.date;
   }
 
+  public toggleSelection() {
+    this.selected = !this.selected;
+  }
+
   public toString() {
     const check = this.selected ? '✔' : ' ';
     return ` ${check} ${this.author} - ${this.title}`;
@@ -24,12 +31,34 @@ class FeedRow implements VideoData {
 }
 
 const Feed: React.FC = () => {
+  const { player } = useContext<Config>(ConfigContext);
   const feed = useContext<VideoData[]>(FetcherContext);
-  const items = useMemo(() => feed.map(item => new FeedRow(item)), [feed]);
-  const rows = items.map(item => item.toString());
+  const items = useMemo<FeedRow[]>(() => feed.map(item => new FeedRow(item)), [feed]);
+  const getRows = useCallback((items: FeedRow[]) => items.map(item => item.toString()), [items])
+  const [rows, setRows] = useState(() => getRows(items));
+  const [index, setIndex] = useState(0);
+
+  const onSelectItem = useCallback((_, i) => setIndex(i), []);
+  const onKey = useCallback(key => {
+    const actions = {
+      ' ': () => items[index].toggleSelection(),
+      'n': () => saveLast(new Date()),
+      'o': () => playVideos(player, [items[index]]),
+      'p': () => {
+        const selected = items.filter(item => item.selected);
+        const dates = selected.map(item => item.date.getTime());
+        saveLast(new Date(Math.max(...dates)));
+        playVideos(player, selected);
+      },
+    };
+    Object.keys(actions).includes(key) && actions[key]();
+    setRows(getRows(items));
+  }, [index, items]);
 
   return (
     <list
+      onSelectItem={onSelectItem}
+      onKeypress={onKey}
       items={rows}
       style={{ selected: { fg: 'green' }}}
       keys
---
+++ src/helpers/player.ts
@@ -0,0 +1,14 @@
+import { spawn } from 'child_process';
+import {VideoData} from './fetchFeed';
+
+let playerProcess;
+
+export const playVideos = (player: string, videos: VideoData[]) => {
+  const links = videos.map(video => video.link);
+
+  playerProcess = spawn(player, links);
+  playerProcess.stdout.on('data', () => {});
+  playerProcess.stderr.on('data', () => {});
+  playerProcess.on('close', () => playerProcess = null);
+};
+
